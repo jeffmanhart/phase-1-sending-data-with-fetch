84df75c62c0cb64c076aeda930e4c1b2aac47217
jeffmanhart <jeffreymanhart@gmail.com>
2023-09-18 10:34:04 -0600

    Created alerting and a post to /users

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
+
+  function submitData(username, userEmail){
+    const userData = {
+       name : username,
+       email : userEmail,
+      };
+
+      const userObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
+
+    return   fetch("http://localhost:3000/users", userObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      //append object to dom
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        div.append(object.id, p);
+        document.querySelector('body').appendChild(div)
+    })
+    .catch(function (error) {
+        //alert("There is an error!");
+        let p = document.createElement("p");
+        p.textContent = error.message;
+        document.querySelector('body').appendChild(p);
+      }
+
+    );
+  }
+  
